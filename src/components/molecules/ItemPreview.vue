828a0833ae98a8f8177c65257a134eda31b90e5f
kisky3 <kisky3@126.com>
2020-01-27 12:18:12 +0900

    iconfont checkを追加　

--- src/components/molecules/ItemPreview.vue
+++ src/components/molecules/ItemPreview.vue
@@ -4,33 +4,49 @@
       <p>商品{{ index + 1 }}</p>
     </div>
     <div class="c-item-container">
-      <span class="iconfont icon-camera item-camera"></span>
+      <img
+        :src="
+          ItemPrevieimage.thumnail ? ItemPrevieimage.thumnail : defaultImage
+        "
+        class="c-item-img"
+        alt=""
+      />
+      <div><span>商品名：</span>{{ item.title }}</div>
     </div>
   </div>
 </template>
 <script>
+import { mapGetters } from "vuex";
+import defaultImage from "../../assets/img/default.png";
 export default {
   name: "ItemPreview",
   props: ["item", "index"],
+  computed: {
+    ...mapGetters({
+      imageList: "itemInformation/getImageList"
+    }),
+    ItemPrevieimage() {
+      return this.imageList[this.index][0];
+    }
+  },
   methods: {}
 };
 </script>
 <style>
 .c-item-container {
   width: 200px;
-  height: 200px;
   margin: 5px 10px 10px;
   border: 2px solid #dedede;
   display: flex;
   justify-content: center;
   align-items: center;
   position: relative;
+  flex-direction: column;
+  background: #fff;
+  padding: 5px;
 }
-
-.item-camera {
-  color: #dedede;
-  font-size: 80px;
-  font-weight: bold;
+.c-item-img {
+  width: 90%;
 }
 
 /* sp */
@@ -38,20 +54,6 @@ export default {
   .c-item-container {
     width: 35vw;
     height: 35vw;
-  }
-
-  .item-camera {
-    color: #dedede;
-    font-size: 60px;
-    font-weight: bold;
-  }
-}
-
-/* ipad */
-@media screen and (min-width: 768px) and (max-width: 1024px) {
-  .c-item-container {
-    width: 200px;
-    height: 200px;
   }
 }
 </style>
