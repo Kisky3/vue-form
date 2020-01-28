47f8b55e187d2c17f60f1457266159a0a42c1697
kisky3 <kisky3@126.com>
2020-01-28 18:13:13 +0900

    ユーザー情報ページに名前を追加する　

--- src/pages/UserInformation.vue
+++ src/pages/UserInformation.vue
@@ -1,14 +1,53 @@
 <template>
-  <div class="item_container">User UserUserUserUserUserUserUserUser</div>
+  <div class="c-page-container">
+    <div class="c-page-title">
+      <p>お客様情報</p>
+    </div>
+    <div class="c-page-row">
+      <div class="c-page-subtitle">
+        <p>お名前（漢字）</p>
+        <require-tag />
+      </div>
+      <input-model
+        v-model="userData.name"
+        type="text"
+        placeholder="例）買取  太郎"
+        value="value"
+        name="name"
+      />
+    </div>
+    <div class="c-page-row">
+      <div class="c-page-subtitle">
+        <p>お名前（カナ）</p>
+        <require-tag />
+      </div>
+      <input-model
+        v-model="userData.kana"
+        type="text"
+        placeholder="例）カイトリ  タロウ"
+        value="value"
+        name="kana"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import InputModel from '../components/atoms/InputModel'
 export default {
-  name: 'UserInformation',
-  props: {},
-};
+  name: "UserInformation",
+  data() {
+    return {
+      userData: {
+        name: null,
+        kana: null
+      }
+    }
+  },
+  components: {
+    InputModel
+  }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
